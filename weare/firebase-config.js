332da452a8cbda9f2e5c58695d2922b08916aef9
EO <eo.willing@gmail.com>
2026-01-21 15:05:27 +0800

    Add Firebase integration for lottery system with database persistence

---
+++ weare/firebase-config.js
@@ -0,0 +1,20 @@
+/**
+ * Firebase 配置檔案 - 尾牙抽獎系統
+ *
+ * 注意：此檔案包含敏感資訊，請勿提交到 Git
+ */
+
+// 定義 Firebase 配置（從 Firebase Console 取得）
+window.__firebase_config = {
+  apiKey: 'AIzaSyDRjxIWJc5BabyvRbhR6uZ8ZaO3J70OKfc',
+  authDomain: 'eoelior-17bed.firebaseapp.com',
+  databaseURL: 'https://eoelior-17bed-default-rtdb.firebaseio.com',
+  projectId: 'eoelior-17bed',
+  storageBucket: 'eoelior-17bed.firebasestorage.app',
+  messagingSenderId: '722816989754',
+  appId: '1:722816989754:web:e6ce3c57198d6bb7769303',
+  measurementId: 'G-53NR19VNB1',
+};
+
+// 定義應用程式識別碼（尾牙抽獎專用）
+window.__app_id = 'lottery-weare';
