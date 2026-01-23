cc5bd97496d7134a511906e02653504b8db813a1
EO <eo.willing@gmail.com>
2026-01-23 08:06:52 +0800

    firebase
    rebox firebase

---
+++ rebox/firebase-config.js
@@ -0,0 +1,9 @@
+window.__firebase_config = {
+  apiKey: "AIzaSyDDgQUZH5N1IaNR2-CeOIHCQjccsJEYD5s",
+  authDomain: "studio-2298218799-c99fe.firebaseapp.com",
+  projectId: "studio-2298218799-c99fe",
+  storageBucket: "studio-2298218799-c99fe.firebasestorage.app",
+  messagingSenderId: "848457307119",
+  appId: "1:848457307119:web:d0c416db3468efb9ef5c1a",
+  databaseURL: "https://studio-2298218799-c99fe-default-rtdb.firebaseio.com"
+};
