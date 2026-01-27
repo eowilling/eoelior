e1cd58bbea60e1c93db7cf691b9fdf047ba38edd
eoelior <eo.willing@gmail.com>
2026-01-27 16:31:04 +0800

    Force Update Firebase config

---
+++ buytonobuy/firebase-config.js
@@ -0,0 +1,12 @@
+window.__firebase_config = {
+  apiKey: "AIzaSyDRjxIWJc5BabyvRbhR6uZ8ZaO3J70OKfc",
+  authDomain: "eoelior-17bed.firebaseapp.com",
+  projectId: "eoelior-17bed",
+  storageBucket: "eoelior-17bed.firebasestorage.app",
+  messagingSenderId: "722816989754",
+  appId: "1:722816989754:web:4ec2a41fe13c96db769303",
+  measurementId: "G-Y0D90RPHNF"
+};
+
+// Application identifier
+window.__app_id = "eoelior-wheel-data";
